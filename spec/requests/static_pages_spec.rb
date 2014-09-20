28f8c933852bd0480af6bd65dbca08acb8ea999c
saldanha.kenneth@gmail.com <saldanha.kenneth@gmail.com>
2014-09-20 20:40:25 +0530

    Changed Static Pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,11 +2,12 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+let(:base_title) {"Consumer Tadka"}
   describe "Home page" do
 
     it "should have the title 'Consumer Tadka | Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title('Home')
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -14,7 +15,7 @@ describe "Static pages" do
 
   it "should have the title 'Consumer Tadka | About'" do
     visit '/static_pages/about'
-    expect(page).to have_title('About')
+    expect(page).to have_title("#{base_title} | About")
   end
 end
 
@@ -22,7 +23,7 @@ end
 
     it "should have the title 'Consumer Tadka | Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title('Help')
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 end
