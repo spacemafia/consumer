c3fa663d2afaadf7084ec676372c665a752406b8
saldanha.kenneth@gmail.com <saldanha.kenneth@gmail.com>
2014-09-21 23:14:48 +0530

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Kenneth Saldanha"
+    email    "kenneth@gmail.com"
+    password "Crisil@123"
+    password_confirmation "Crisil@123"
+  end
+end
