3b1993e151d7e343cba80fa6e43210199533404c
saldanha.kenneth@gmail.com <saldanha.kenneth@gmail.com>
2014-09-23 22:18:02 +0530

    Added updating and edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -10,6 +13,7 @@ class UsersController < ApplicationController
   def create
   @user = User.new(user_params)
   if @user.save
+    sign_in @user
     flash[:success] = "Welcome to Consumer Tadka!"
   	redirect_to @user
   else
@@ -17,10 +21,39 @@ class UsersController < ApplicationController
   end
 end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+        # Before filters
+
+    def signed_in_user
+      unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in." 
+    end
+  end
+   
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
