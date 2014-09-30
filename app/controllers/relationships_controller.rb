b546b0f2defc0105f482226505be718615bbe830
saldanha.kenneth@gmail.com <saldanha.kenneth@gmail.com>
2014-09-30 15:34:54 +0530

    Corrected follower routing mistake

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,12 +4,12 @@ class RelationshipsController < ApplicationController
   def create
     @question = Question.find(params[:relationship][:followed_id])
     current_user.follow_question!(@question)
-    redirect_to root_path
+    redirect_to user_question_path(:email => @question.user.id, :id => @question.id)
   end
 
   def destroy
     @question = Relationship.find(params[:id]).followed
     current_user.question_unfollow!(@question)
-    redirect_to root_path
+    redirect_to user_question_path(:email => @question.user.id, :id => @question.id)
     end
 end
